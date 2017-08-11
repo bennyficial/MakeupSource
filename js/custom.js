ce17a75b637e5883f33e287a8585b3b3b2904431
bennyficial <benny.u.lee@gmail.com>
2017-08-10 20:30:39 -0700

    fixed moble nav

--- js/custom.js
+++ js/custom.js
@@ -28,17 +28,17 @@ jQuery(document).ready(function( $ ) {
       $('body').append( $mobile_nav );
       $('body').prepend( '<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>' );
       $('body').append( '<div id="mobile-body-overly"></div>' );
-      $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
+      // $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
       
-      $(document).on('click', '.menu-has-children i', function(e){
+      $(document).on('click', '.menu-has-children a', function(e){
           $(this).next().toggleClass('menu-item-active');
           $(this).nextAll('ul').eq(0).slideToggle();
-          $(this).toggleClass("fa-chevron-up fa-chevron-down");
+          // $(this).toggleClass("fa-chevron-up fa-chevron-down");
       });
       
       $(document).on('click', '#mobile-nav-toggle', function(e){
           $('body').toggleClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-nav-toggle a').toggleClass('fa-times fa-bars');
           $('#mobile-body-overly').toggle();
       });
       
@@ -47,7 +47,7 @@ jQuery(document).ready(function( $ ) {
           if (!container.is(e.target) && container.has(e.target).length === 0) {
              if ( $('body').hasClass('mobile-nav-active') ) {
                   $('body').removeClass('mobile-nav-active');
-                  $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+                  $('#mobile-nav-toggle a').toggleClass('fa-times fa-bars');
                   $('#mobile-body-overly').fadeOut();
               }
           }
